57a837adc83971ec77d1dfca56f71914703cf22b
amirhossein-github <amirhossein.business.developer@gmail.com>
2024-08-31 16:47:04 +0330

    feat: add section variables
    Description:
    The section related to variables was added:
    1. Naming variables
    2. Global and Local Variable
    3. How to get variable name
    
    Change Log:
    [variable][make]: Creating a file and adding information about variables

---
+++ javascript-database/variable.js
@@ -0,0 +1,44 @@
+/**
+ * Variable in javascript
+ */
+
+//* Naming variables
+//* Standard format
+{
+    // Start With: 'Dollar Sign', 'lower and upper case letter', 'underscore'
+    const $variableName = ''
+    const _variableName = ''
+    const variableName = ''
+    const VariableName = ''
+
+    /* can't be
+        Space: mom dad = 'family'
+        Reserved Keywords: [if, else, NaN, null, e.g.]
+        Start With Number: 12Name = 'ali'
+        Using Symbol: symbol%: 'test 1 2 3'
+    */
+
+    // format
+    const snake      = 'amir_hossein_khateri'
+    const camelCase  = 'amirHosseinKhateri'
+    const PascalCase = 'AmirHosseinKhateri'
+    const UPPERCASE  = 'AMIRHOSSEINKHATERI'
+    const lowercase  = 'amirhosseinkhateri'
+}
+
+//* Global and Local Variable
+{
+    // Its outside of all scope
+    const globalVariable = '3 2 1'
+
+    {
+        // local variable inside of scope
+        const localVariable = '1 2 3'
+    }
+}
+
+//* How to get variable name
+{
+    const variable = ':)'
+    Object.keys({variable})[0]
+}
